a45c0aacc94f3cd32af0f6c6dc5a1d35dc69cf8d
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-12-05 20:19:18 -0300

    solution(file-metadata-microservice): update index.js

--- projects/file-metadata-microservice/index.js
+++ projects/file-metadata-microservice/index.js
@@ -1,18 +1,28 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
+app.use(express.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {  
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
 
-
+  next();
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
